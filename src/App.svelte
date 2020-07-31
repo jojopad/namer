012e7775454452744d60404ec6275c2f04f6f639
jojopad <jojo.paderes@gmail.com>
2020-07-31 16:06:08 +0800

    Copy to clipboard added.

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,53 @@
 <script>
-	import Chance from 'chance'
+	import Chance from 'chance';
+	import Copy from './Copy.svelte'
+	import { onMount } from 'svelte';
+
 	let chance = new Chance()
-	let company = chance.company()
-	let firstName = chance.first()
-	let lastName = chance.last()
-	let email = chance.string({ length: 10, alpha: true, numeric: true }) + '@yopmail.com'
-	let guid = chance.guid()
-	let hash = chance.hash()
+	let company, name, firstName, lastName, email, guid, hash;
+
+	function generate() {
+		company = chance.company();
+		firstName = chance.first();
+		lastName = chance.last();
+		let emailTmp = firstName + lastName + chance.string({ length: 10, alpha: true, numeric: true }) + '@yopmail.com';
+		email = emailTmp.replace(/\s/g, '');
+		guid = chance.guid();
+		hash = chance.hash();
+	}
+
+	function copy(value) {
+		navigator.clipboard.writeText(value);
+	}
+
+	onMount(async () => generate());
 </script>
 
-<h3>Company</h3>
-<span>{company}</span>
-<h3>Name</h3>
-<span>{firstName} {lastName}</span>
-<h3>Email</h3>
-<span>{email}</span>
-<h3>GUID</h3>
-<span>{guid}</span>
-<h3>Hash</h3>
-<span>{hash}</span>
+<div style="margin-left: 50px; margin-top: 50px">
+	<button on:click="{generate}">Generate</button>
+
+	<h3>Company</h3>
+	<Copy value="{company}"/>
+	<span>{company}</span>
+
+
+	<h3>First Name</h3>
+	<Copy value="{firstName}"/>
+	<span>{firstName}</span>
+
+	<h3>Last Name</h3>
+	<Copy value="{lastName}"/>
+	<span>{lastName}</span>
+
+	<h3>Email</h3>
+	<Copy value="{email}"/>
+	<span>{email}</span>
+
+	<h3>GUID</h3>
+	<Copy value="{guid}"/>
+	<span>{guid}</span>
+
+	<h3>Hash</h3>
+	<Copy value="{hash}"/>
+	<span>{hash}</span>
+</div>
---
+++ src/Copy.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { NotificationDisplay, notifier } from '@beyonk/svelte-notifications'
+
+    export let value = '';
+
+    function copy() {
+        navigator.clipboard.writeText(value);
+        notifier.info(`Copied!`);
+    }
+</script>
+
+<NotificationDisplay />
+
+<button
+        style="margin-right: 5px"
+        title="Copy to clipboard"
+        on:click={copy}>
+    <span class="jam jam-clipboard"></span>
+</button>
