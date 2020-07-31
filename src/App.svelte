c38caa62c717f465fc8e916e2e84752a5af2df43
jojopad <jojo.paderes@gmail.com>
2020-07-31 14:44:26 +0800

    Added chance package for generating random company/person name and email address.

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,14 @@
 <script>
-	let name = 'what';
+	import Chance from 'chance';
+	let chance = new Chance();
+	let company = chance.company();
+	let name = chance.name();
+	let email = chance.string({ length: 10, alpha: true, numeric: true }) + '@yopmail.com';
 </script>
 
-<h1>My name is {name}!</h1>
+<h3>Company</h3>
+<span>{company}</span>
+<h3>Name</h3>
+<span>{name}</span>
+<h3>Email</h3>
+<span>{email}</span>
