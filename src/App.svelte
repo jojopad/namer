52ff1224d65069c0e2aa2ce6965531ec74141dba
jojopad <jojo.paderes@gmail.com>
2020-07-31 14:55:24 +0800

    Generate first and last name separately. GUID and hash values added.

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,21 @@
 <script>
-	import Chance from 'chance';
-	let chance = new Chance();
-	let company = chance.company();
-	let name = chance.name();
-	let email = chance.string({ length: 10, alpha: true, numeric: true }) + '@yopmail.com';
+	import Chance from 'chance'
+	let chance = new Chance()
+	let company = chance.company()
+	let firstName = chance.first()
+	let lastName = chance.last()
+	let email = chance.string({ length: 10, alpha: true, numeric: true }) + '@yopmail.com'
+	let guid = chance.guid()
+	let hash = chance.hash()
 </script>
 
 <h3>Company</h3>
 <span>{company}</span>
 <h3>Name</h3>
-<span>{name}</span>
+<span>{firstName} {lastName}</span>
 <h3>Email</h3>
 <span>{email}</span>
+<h3>GUID</h3>
+<span>{guid}</span>
+<h3>Hash</h3>
+<span>{hash}</span>
